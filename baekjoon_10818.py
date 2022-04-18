b21f8df5b8c25aa5bfe673c7cc5c27ccc82231f8
HOIUH <46985003+HOIUH@users.noreply.github.com>
2022-04-18 22:42:35 +0900

    baekjoon_10818

---
+++ baekjoon_10818.py
@@ -0,0 +1,17 @@
+# 20220418
+# 최소, 최대
+# https://www.acmicpc.net/problem/10818
+
+input_cnt = int(input())
+
+input = list(map(int, input().split()))
+
+print(min(input), max(input))
+
+
+
+
+
+
+
+
