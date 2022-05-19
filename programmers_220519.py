6e624c00149db4e7f21719f55221526218637c8c
HOIUH <46985003+HOIUH@users.noreply.github.com>
2022-05-19 22:22:58 +0900

    Update programmers_220519.py
    1. PYTHON heapq 모듈 사용

--- programmers_220519.py
+++ programmers_220519.py
@@ -21,10 +21,8 @@ def solution(scoville, K):
         # heap[0]에 가장 작은 원소가 있다고 해서, heap[1]에 두번째 작은 원소가 있다는 보장 없음
         # heappop() 함수로 가장 작은 원소 삭제할때마다 이진 트리 재배치를 통해 매번 새로운 최소값을 인덱스 0에 위치시키기 때문
         # 따라서, 두번째로 작은 원소 얻으려면 heap[1]로 접근하면 안된다!!
-        first_min = scoville[0]
-        heapq.heappop(scoville)
-        second_min = scoville[0]
-        heapq.heappop(scoville)
+        first_min = heapq.heappop(scoville)
+        second_min = heapq.heappop(scoville)
         new_s = first_min + (second_min * 2)
         heapq.heappush(scoville, new_s)
         answer += 1
