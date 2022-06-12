faabb5933da24e865457d6cb0d7d2619f265e4ed
HOIUH <46985003+HOIUH@users.noreply.github.com>
2022-06-12 21:44:35 +0900

    Palindrome Linked List
    1. Singly-linked List into List
    2. Singly-linked List into collections.deque()
    3. List의 pop(0) 과 deque의 popleft()

---
+++ leetcode_234.py
@@ -0,0 +1,52 @@
+# 220612
+# Palindrome Linked List
+# https://leetcode.com/problems/palindrome-linked-list/
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+
+        # 풀이2 데크를 이용한 최적화
+        q = collections.deque()
+
+        if not head: return True
+
+        node = head
+        while node is not None:
+            q.append(node.val)
+            node = node.next
+
+        while len(q) > 1:
+            if q.popleft() != q.pop():
+                return False
+
+        return True
+
+        """
+        # 풀이1 리스트 변환
+        q = []
+
+        if not head:
+            return True
+
+        node = head
+        # 리스트 변환
+        while node is not None:
+            q.append(node.val)
+            node = node.next
+
+        # 팰린드롬 판별
+        while len(q) > 1:
+            if q.pop(0) != q.pop():
+                return False
+
+        return True
+        """
