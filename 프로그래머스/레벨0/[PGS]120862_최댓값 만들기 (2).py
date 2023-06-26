9d4cbfee6d03322eba73ba3f921b18c6a652768e
HOIUH <46985003+HOIUH@users.noreply.github.com>
2023-06-27 08:31:59 +0900

    [PGS] 최댓값 만들기 (2) / 레벨0
    https://school.programmers.co.kr/learn/courses/30/lessons/120862

---
+++ 프로그래머스/레벨0/[PGS]120862_최댓값 만들기 (2).py
@@ -0,0 +1,7 @@
+# 230627
+# [PGS] 최댓값 만들기 (2) / 레벨0
+# https://school.programmers.co.kr/learn/courses/30/lessons/120862
+
+def solution(numbers):
+    numbers.sort()
+    return max(numbers[0] * numbers[1], numbers[-2] * numbers[-1])
