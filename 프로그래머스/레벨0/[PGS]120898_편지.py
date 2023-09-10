823c1aa09f4910f42d35cb8565e16a304ba77b0b
HOIUH <46985003+HOIUH@users.noreply.github.com>
2023-09-10 15:39:53 +0900

    [PGS] 편지 / 레벨0
    https://school.programmers.co.kr/learn/courses/30/lessons/120898

---
+++ 프로그래머스/레벨0/[PGS]120898_편지.py
@@ -0,0 +1,10 @@
+# 230910
+# [PGS] 편지 / 레벨0
+# https://school.programmers.co.kr/learn/courses/30/lessons/120898
+
+def solution(message):
+    return len(message) * 2
+
+
+print(solution("happy birthday!"))
+print(solution("I love you~"))
