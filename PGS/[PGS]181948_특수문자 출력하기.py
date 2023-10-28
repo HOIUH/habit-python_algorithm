6e1f1abac7154ed09b49cfeba829c63c4359d05e
HOIUH <46985003+HOIUH@users.noreply.github.com>
2023-10-29 00:52:19 +0900

    [PGS] 특수문자 출력하기 / 레벨0
    https://school.programmers.co.kr/learn/courses/30/lessons/181948

---
+++ PGS/[PGS]181948_특수문자 출력하기.py
@@ -0,0 +1,7 @@
+# 231029
+# [PGS] 특수문자 출력하기 / 레벨0
+# https://school.programmers.co.kr/learn/courses/30/lessons/181948
+
+# print("!@#$%^&*(\\'\"<>?:;")
+# print('''!@#$%^&*(\\'"<>?:;''')
+print(r'!@#$%^&*(\'"<>?:;')
