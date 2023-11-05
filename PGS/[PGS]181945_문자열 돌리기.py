4f3011c7ef6034ac45c7fa237b62bcde8612737a
HOIUH <46985003+HOIUH@users.noreply.github.com>
2023-11-05 15:33:16 +0900

    [PGS] 문자열 돌리기 / 레벨0
    https://school.programmers.co.kr/learn/courses/30/lessons/181945

---
+++ PGS/[PGS]181945_문자열 돌리기.py
@@ -0,0 +1,15 @@
+# 231105
+# [PGS] 문자열 돌리기 / 레벨0
+# https://school.programmers.co.kr/learn/courses/30/lessons/181945
+
+# 풀이 1
+# for문 사용
+'''
+str = input()
+for s in str:
+    print(s)
+'''
+
+# 풀이 2
+# '구분자'.join(iterable) 사용
+print('\n'.join(input()))
