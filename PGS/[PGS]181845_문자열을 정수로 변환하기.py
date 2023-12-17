4ebd058732ddd8f85bc4f834f4d3064a4af71344
HOIUH <46985003+HOIUH@users.noreply.github.com>
2023-12-17 15:53:55 +0900

    [PGS] 문자열을 정수로 변환하기 / 레벨0
    https://school.programmers.co.kr/learn/courses/30/lessons/181848

---
+++ PGS/[PGS]181845_문자열을 정수로 변환하기.py
@@ -0,0 +1,10 @@
+# 231217
+# [PGS] 문자열을 정수로 변환하기 / 레벨0
+# https://school.programmers.co.kr/learn/courses/30/lessons/181848
+
+# 풀이 1
+def solution(n_str):
+    return int(n_str)
+
+# 풀이 2
+# solution = int
