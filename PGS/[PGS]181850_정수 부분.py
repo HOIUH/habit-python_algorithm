c621d79ad9dce29b041cb09856f31da4abadb10d
HOIUH <46985003+HOIUH@users.noreply.github.com>
2024-01-07 09:52:02 +0900

    [PGS] 정수 부분 / 레벨0
    https://school.programmers.co.kr/learn/courses/30/lessons/181850

---
+++ PGS/[PGS]181850_정수 부분.py
@@ -0,0 +1,13 @@
+# 240107
+# [PGS] 정수 부분 / 레벨0
+# https://school.programmers.co.kr/learn/courses/30/lessons/181850
+
+def solution(flo):
+    # 풀이 1
+    # int 메소드 사용
+    #return int(flo)
+
+    # 풀이 2
+    # 1로 나눴을때의 몫만 사용
+    return flo//1
+
