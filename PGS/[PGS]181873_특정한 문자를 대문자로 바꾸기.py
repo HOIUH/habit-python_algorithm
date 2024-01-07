54e8efe4879424074d97c137a8468bcdca34f15a
HOIUH <46985003+HOIUH@users.noreply.github.com>
2024-01-07 09:50:40 +0900

    [PGS] 특정한 문자를 대문자로 바꾸기 / 레벨0
    https://school.programmers.co.kr/learn/courses/30/lessons/181873

---
+++ PGS/[PGS]181873_특정한 문자를 대문자로 바꾸기.py
@@ -0,0 +1,6 @@
+# 240107
+# [PGS] 특정한 문자를 대문자로 바꾸기 / 레벨0
+# https://school.programmers.co.kr/learn/courses/30/lessons/181873
+
+def solution(my_string, alp):
+    return my_string.replace(alp, alp.upper())
