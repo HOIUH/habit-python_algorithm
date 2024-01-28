cd89dbac8038cc3dd9efe26f6e22264ac75b19e2
HOIUH <46985003+HOIUH@users.noreply.github.com>
2024-01-28 11:58:36 +0900

    [PGS] 특정 문자 제거하기 / 레벨0
    https://school.programmers.co.kr/learn/courses/30/lessons/120826

---
+++ PGS/[PGS]120826_특정 문자 제거하기.py
@@ -0,0 +1,8 @@
+# 240128
+# [PGS] 특정 문자 제거하기 / 레벨0
+# https://school.programmers.co.kr/learn/courses/30/lessons/120826
+
+
+def solution(my_string, letter):
+    return my_string.replace(letter, '')
+
