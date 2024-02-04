f7a8ced2a7ef9a85a07f13b8d9e558203a020fda
HOIUH <46985003+HOIUH@users.noreply.github.com>
2024-02-04 14:12:07 +0900

    [PGS] 중복된 숫자 개수 / 레벨0
    https://school.programmers.co.kr/learn/courses/30/lessons/120583

---
+++ PGS/[PGS]120583_중복된 숫자 개수.py
@@ -0,0 +1,10 @@
+# 240204
+# [PGS] 중복된 숫자 개수 / 레벨0
+# https://school.programmers.co.kr/learn/courses/30/lessons/120583
+
+
+def solution(array, n):
+    return array.count(n)
+
+print(solution([1, 1, 2, 3, 4, 5], 1))
+print(solution([0, 2, 3, 4], 1))
