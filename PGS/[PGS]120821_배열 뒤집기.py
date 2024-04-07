fa42992962481b0559f7d0e13657378ebca544b5
HOIUH <46985003+HOIUH@users.noreply.github.com>
2024-04-07 14:36:20 +0900

    [PGS] 배열 뒤집기/ 레벨0
    https://school.programmers.co.kr/learn/courses/30/lessons/120821

---
+++ PGS/[PGS]120821_배열 뒤집기.py
@@ -0,0 +1,11 @@
+# 240407
+# [PGS] 배열 뒤집기/ 레벨0
+# https://school.programmers.co.kr/learn/courses/30/lessons/120821
+
+def solution(num_list):
+    # 풀이 1
+    return num_list[::-1]
+
+    # 풀이 2
+    # num_list.reverse()
+    # return num_list
