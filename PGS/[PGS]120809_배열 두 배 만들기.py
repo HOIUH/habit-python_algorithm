6cead9f02821ec132b4f0801f6891a1814733b0a
HOIUH <46985003+HOIUH@users.noreply.github.com>
2024-04-07 14:35:36 +0900

    [PGS] 배열 두 배 만들기 / 레벨0
    https://school.programmers.co.kr/learn/courses/30/lessons/120809

---
+++ PGS/[PGS]120809_배열 두 배 만들기.py
@@ -0,0 +1,7 @@
+# 240407
+# [PGS] 배열 두 배 만들기 / 레벨0
+# https://school.programmers.co.kr/learn/courses/30/lessons/120809
+
+def solution(numbers):
+    # List Comprehension 사용
+    return [num*2 for num in numbers]
