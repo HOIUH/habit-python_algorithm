45ed882899cc6cd61771584271857a3b36513a8e
HOIUH <46985003+HOIUH@users.noreply.github.com>
2024-04-14 15:56:15 +0900

    [PGS] 몫 구하기 / 레벨0
    https://school.programmers.co.kr/learn/courses/30/lessons/120805

---
+++ PGS/[PGS]120805_몫 구하기.py
@@ -0,0 +1,11 @@
+# 240414
+# [PGS] 몫 구하기 / 레벨0
+# https://school.programmers.co.kr/learn/courses/30/lessons/120805
+
+# 풀이 1
+# def solution(num1, num2):
+    # return num1//num2
+
+# 풀이 2
+solution = lambda x, y : x//y
+
